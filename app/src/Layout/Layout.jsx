339b77aea03f282f6fd9500d6c895a007deb55ec
Matt Szabo <wekilledit@users.noreply.github.com>
2016-06-19 18:16:12 +1000

    added smooth scrool to layout for the anchor tags

--- app/src/Layout/Layout.jsx
+++ app/src/Layout/Layout.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import $ from 'jquery'
+
 import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer'
 
@@ -7,6 +9,21 @@ import About    from '../About/About';
 import Projects from '../Projects/components/Projects';
 import Skills   from '../Skills/components/Skills'
 import Contact  from '../Contact/components/Contact'
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
 
 export default class Layout extends React.Component {
   render() {
