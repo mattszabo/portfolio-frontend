3284f698ffc83d43d680e9871e178f8dba6291fc
Matt Szabo <wekilledit@users.noreply.github.com>
2016-07-05 12:24:10 +1000

    completed rough home page

--- app/src/Home/Home.jsx
+++ app/src/Home/Home.jsx
@@ -5,8 +5,8 @@ import './home.sass'
 const Home = () => {
   return(
     <section className='page-home' id='home'>
-      <div className='my-image'>
-        <h1>Matt</h1>
+      <div>
+        <h2>Matt Szabo</h2>
         <img src={require('./images/hire-me.jpg')} />
       </div>
     </section>
