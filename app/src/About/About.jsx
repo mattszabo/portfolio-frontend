ea5f0a88e2aad33cb12bdab6d21b5a74d10222fe
Matt Szabo <wekilledit@users.noreply.github.com>
2016-07-08 15:49:35 +1000

    adding swiping to carousel, which is now used in about page

--- app/src/About/About.jsx
+++ app/src/About/About.jsx
@@ -1,37 +1,44 @@
 import React from 'react'
 
+import Carousel from '../Carousel/Carousel'
 import './about.sass'
 
 const About = () => {
   return (
     <section className='page-about' id='about'>
       <h2>AbouT</h2>
-      <p>
-        <img src={require('./images/hire-me.jpg')} />
-        I come from a DevOps background, where I supported a large Java application with an SQL backend, all hosted by Amazon on an AWS EC2 app server and Oracle RDS instance. To be effective at that role I utilised my <strong>Java</strong>, <strong>SQL</strong>, and <strong>UNIX tools</strong> knowledge and developed exceptional <strong>problem solving</strong> skills. Another key asset learned during my time in that role was the ability to <strong>rapidly learn</strong> new technolgies in an ever changing world of programming techniques and languages.
-      </p>
-      <p>
-        Since my time in DevOps concluded, I have started fresh with front end web development and its related technologies since the beginning of May, 2016.
-      </p>
-      <p>
-        This single page app demonstates much of what I have learned so far. It was developed using <strong>webpack</strong>, <strong>git</strong>, <strong>npm</strong>, <strong>chrome dev tools</strong>, and <strong>React dev tools (for chrome)</strong>. It is written in <strong>JavaScript ES6</strong>, which was transpiled using <strong>babel</strong>, uses <strong>React</strong> components and <strong>Sass</strong> for styling. The app is also hosted on a <strong>Node.js</strong> express server, which resides on an AWS EC2 server housed in Sydney, Australia.
-      </p>
-      <p>
-        Spefically, since I began I have focussed on developing the following skills:
-      </p>
-      <ul>
-        <li>JavaScript ES6</li>
-        <li>CSS/Sass</li>
-        <li>React</li>
-        <li>Redux</li>
-        <li>Webpack (w/ hot reloading)</li>
-        <li>Node.js</li>
-        <li>Git</li>
-        <li>React/redux/chrome dev tools</li>
-        <li>Mobile first design</li>
-      </ul>
+      <Carousel buttonStyle='button-side'>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </Carousel>
     </section>
   )
 }
 
 export default About
+
+{/*<p>
+  <img src={require('./images/hire-me.jpg')} />
+  I come from a DevOps background, where I supported a large Java application with an SQL backend, all hosted by Amazon on an AWS EC2 app server and Oracle RDS instance. To be effective at that role I utilised my <strong>Java</strong>, <strong>SQL</strong>, and <strong>UNIX tools</strong> knowledge and developed exceptional <strong>problem solving</strong> skills. Another key asset learned during my time in that role was the ability to <strong>rapidly learn</strong> new technolgies in an ever changing world of programming techniques and languages.
+</p>
+<p>
+  Since my time in DevOps concluded, I have started fresh with front end web development and its related technologies since the beginning of May, 2016.
+</p>
+<p>
+  This single page app demonstates much of what I have learned so far. It was developed using <strong>webpack</strong>, <strong>git</strong>, <strong>npm</strong>, <strong>chrome dev tools</strong>, and <strong>React dev tools (for chrome)</strong>. It is written in <strong>JavaScript ES6</strong>, which was transpiled using <strong>babel</strong>, uses <strong>React</strong> components and <strong>Sass</strong> for styling. The app is also hosted on a <strong>Node.js</strong> express server, which resides on an AWS EC2 server housed in Sydney, Australia.
+</p>
+<p>
+  Spefically, since I began I have focussed on developing the following skills:
+</p>
+<ul>
+  <li>JavaScript ES6</li>
+  <li>CSS/Sass</li>
+  <li>React</li>
+  <li>Redux</li>
+  <li>Webpack (w/ hot reloading)</li>
+  <li>Node.js</li>
+  <li>Git</li>
+  <li>React/redux/chrome dev tools</li>
+  <li>Mobile first design</li>
+</ul>*/}
--- app/src/Carousel/Carousel.jsx
+++ app/src/Carousel/Carousel.jsx
@@ -1,71 +1,75 @@
 import React, { PropTypes } from 'react';
-import classNames from 'classnames';
+// import classNames from 'classnames';
+import SwipeableViews from 'react-swipeable-views';
 
 import './carousel.sass';
 
 class Carousel extends React.Component {
   componentWillMount() {
-    this.setState({
-      currentId: 1
-    })
+    this.setState({ index: 1 })
   }
   prevButtonClick = () => {
     let numItems = this.props.children.length;
-    let currentId = this.state.currentId;
-    let newId;
+    let index = this.state.index;
+    let newIndex;
 
-    if (currentId === 0) {
-      newId = numItems - 1;
-    } else {
-      newId = currentId - 1;
-    }
-
-    this.setState({
-      currentId: newId
-    })
+    newIndex = index === 0 ? numItems - 1 : index - 1;
+    this.setState({ index: newIndex })
   }
   nextButtonClick = () => {
     let numItems = this.props.children.length;
-    let currentId = this.state.currentId;
-    let newId;
+    let index = this.state.index;
+    let newIndex;
 
-    if (currentId === numItems - 1) {
-      newId = 0;
-    } else {
-      newId = currentId + 1;
-    }
-
-    this.setState({
-      currentId: newId
-    })
+    newIndex = index === numItems - 1 ? 0 : newIndex = index + 1;
+    this.setState({ index: newIndex })
   }
+  handleChangeIndex = (index) => {
+    this.setState({
+      index: index
+    });
+  };
   render() {
+    let buttonClass = this.props.buttonStyle || 'button-top';
+    console.log(buttonClass);
     return (
 
-      <div className='project-carousel carousel-active'>
-        <div className='button-area'>
-          <button onClick={this.prevButtonClick}>
-            &lt;<span className='offscreen'>Previous</span>
-          </button>
-          <button onClick={this.nextButtonClick}>
-            <span className='offscreen'>Next</span>&gt;
-          </button>
+      // <div className='project-carousel carousel-active'>
+      //   <div className={buttonClass}>
+      //     <button className='button-prev' onClick={this.prevButtonClick}>
+      //       &lt;<span className='offscreen'>Previous</span>
+      //     </button>
+      //     <button className='button-next' onClick={this.nextButtonClick}>
+      //       <span className='offscreen'>Next</span>&gt;
+      //     </button>
+      //   </div>
+      //     <ol className='project-carousel-content'>
+      //       {this.props.children.map((item, i) => {
+      //         const _class = classNames(
+      //           {'current': i === this.state.currentId}
+      //         );
+      //         return (
+      //           <li className={_class} key={i}>
+      //             {item}
+      //           </li>
+      //         )
+      //       })}
+      //     </ol>
+      // </div>
+      <div>
+        <SwipeableViews index={this.state.index} onChangeIndex={this.handleChangeIndex}>
+          {this.props.children}
+        </SwipeableViews>
+        <div className='dots'>
+          <div className={buttonClass}>
+            <button className='button-prev' onClick={this.prevButtonClick}>
+              &lt;<span className='offscreen'>Previous</span>
+            </button>
+            <button className='button-next' onClick={this.nextButtonClick}>
+              <span className='offscreen'>Next</span>&gt;
+            </button>
+          </div>
         </div>
-
-        <ol className='project-carousel-content'>
-          {this.props.children.map((item, i) => {
-            const _class = classNames({
-              'current': i === this.state.currentId
-            });
-            return (
-              <li className={_class}>
-                <CarouselItem key={i} i={i}>
-                  {item}
-                </CarouselItem>
-              </li>
-            )
-          })}
-        </ol>
       </div>
     );
   }
@@ -75,23 +79,8 @@ Carousel.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node
-  ])
+  ]),
+  buttonStyle: PropTypes.string
 }
 
-class CarouselItem extends React.Component {
-  render() {
-    return(
-      <div>{this.props.children}</div>
-    );
-  }
-}
-
-CarouselItem.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
-  ])
-}
-
-
-export { Carousel as default, CarouselItem };
+export default Carousel;
