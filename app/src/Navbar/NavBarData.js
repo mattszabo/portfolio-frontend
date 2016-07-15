85094a834857d04af5a55899dbd7c12422b7ee86
Matt szabo <wekilledit@users.noreply.github.com>
2016-07-15 23:19:30 +1000

    create resources and moved carousel arrows

--- app/src/Navbar/NavBarData.js
+++ app/src/Navbar/NavBarData.js
@@ -6,11 +6,11 @@ export default class NavBarData {
 
   static getNavBarList() {
     return [
-        {id: 0, url: '#home'      , text: 'home'       },
-        {id: 1, url: '#about'     , text: 'AbouT'      },
-        {id: 2, url: '#projects'  , text: 'projecTs'   },
-        {id: 3, url: '#references', text: 'references' },
-        {id: 4, url: '#contact'   , text: 'conTAcT'    }
+        {id: 0, url: '#home'      , text: 'home'      },
+        {id: 1, url: '#about'     , text: 'AbouT'     },
+        {id: 2, url: '#projects'  , text: 'projecTs'  },
+        {id: 3, url: '#resources' , text: 'resources' },
+        {id: 4, url: '#contact'   , text: 'conTAcT'   }
     ];
   }
 }
