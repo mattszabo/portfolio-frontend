1f3025a819882cc2a420613c13a77bb47b10d482
Matt szabo <wekilledit@users.noreply.github.com>
2016-07-18 17:17:45 +1000

    created resources page. fixes #50

--- app/src/Resources/Resources.jsx
+++ app/src/Resources/Resources.jsx
@@ -1,68 +1,45 @@
 import React from 'react'
 
+import { ReactLogo, ReduxLogo, WebpackLogo, JSLogo, CSS3Logo } from './images/Logos';
+import ResourceLinks from './ResourceLinks';
 import './resources.sass'
 
-const Resources = () => (
-  <section className='page-resources' id='resources'>
-    <h2>rEsourcEs</h2>
-    <p>
-      Code School and Stack Overflow were my main sources for learning. However, these links really helped me and maybe they'll help you too.
-    </p>
-    <ul>
-      <li>
-        I was introduced to this
-        <a href='https://daveceddia.com/timeline-for-learning-react/'>
-          timeline for learning React
-        </a>
-        early on and it helped me to slow down and pace myself for all that I was about to learn
-      </li>
-      <li>
-        SurviveJS has very informative
-        <a href='http://survivejs.com/webpack/'>
-          webpack guide
-        </a>
-        and also a
-        <a href='http://survivejs.com/react/'>
-          react guide
-        </a>
-        that I often used as a reference
-      </li>
-      <li>
-        Wes Bos has a great guide for
-        <a href='https://learnredux.com/'>
-          learning redux
-        </a>
-        that I followed and used as the basis for my redux counter project
-      </li>
-      <li>
-        When I was in need of style inspiration I often turned to
-        <a href='http://tympanus.net/codrops/'>
-          Codrops
-        </a>
-      </li>
-      <li>
-        When looking at online examples of ES5 code this
-        <a href='https://babeljs.io/blog/2015/06/07/react-on-es6-plus'>
-          babeljs blog
-        </a>
-        helped me understand them in ES6+ code
-      </li>
-      <li>
-        Starting out with redux, I turned to this cartoon guide to
-        <a href='https://code-cartoons.com/a-cartoon-guide-to-flux-6157355ab207#.lzupzyb8r'>
-          flux
-        </a>
-        to help understand what idea redux was built upon and the problem it solves
-      </li>
-      <li>
-        This cartoon guide to
-        <a href='https://code-cartoons.com/a-cartoon-intro-to-redux-3afb775501a6#.u0jr6w64z'>
-          redux
-        </a>
-        helped to understand how redux solved the problems it saw in flux
-      </li>
-    </ul>
-  </section>
-)
+class Resources extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={topicToDisplay: 'react'}
+  }
+  displayLinks = (text) => {
+    this.setState({
+      topicToDisplay: text
+    });
+  }
+  render() {
+    return (
+      <section className='page-resources' id='resources'>
+        <h2>rEsourcEs</h2>
+        <p>
+          Other than Code School and Stack Overflow, there were a few other reasources that helped me out. Maybe they'll help you too.
+        </p>
+        <p className='resrouces-hint'>
+          (Click on the logos below to change the topic being displayed)
+        </p>
+        <ul className='resource-topics'>
+          <li onClick={this.displayLinks.bind(this, 'react')}>
+            <ReactLogo    /></li>
+          <li onClick={this.displayLinks.bind(this, 'redux')}>
+            <ReduxLogo    /></li>
+          <li onClick={this.displayLinks.bind(this, 'webpack')}>
+            <WebpackLogo  /></li>
+          <li onClick={this.displayLinks.bind(this, 'js')}>
+            <JSLogo       /></li>
+          <li onClick={this.displayLinks.bind(this, 'css')}>
+            <CSS3Logo     /></li>
+        </ul>
+        <ResourceLinks topicToDisplay={this.state.topicToDisplay} />
+      </section>
+    );
+  }
+}
 
 export default Resources
