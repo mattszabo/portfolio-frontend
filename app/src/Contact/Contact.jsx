327e9c10476ac73c7396d8a3e33cae7bf5111172
Matt Szabo <wekilledit@users.noreply.github.com>
2016-08-01 17:37:54 +1000

    input working with haruki style. need to refactor

--- app/src/Contact/Contact.jsx
+++ app/src/Contact/Contact.jsx
@@ -1,19 +1,55 @@
 import React from 'react'
+import className from 'classname';
 
 import { GmailLogo, GitHubLogo, LinkedInLogo } from './ContactLogos'
 import './contact.sass'
 
-const Contact = () => (
-  <section className='page-contact' id='contact'>
-    <h2>conTAcT</h2>
-    <div className='contact-icons'>
-      <ul>
-        <li><GmailLogo /></li>
-        <li><LinkedInLogo /></li>
-        <li><GitHubLogo /></li>
-      </ul>
-    </div>
-  </section>
-)
+class Contact extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      contactName: ''
+    }
+    console.log('state: ', this.state)
+  }
+  handleNameChange = (e) => {
+    this.setState({
+      contactName: e.target.value
+    })
+    console.log('state: ', this.state)
+  }
+  render() {
+    const contactNameClass = className({
+      'input': true,
+      'input--haruki': true,
+      'input--filled': this.state.contactName.length > 0
+    })
+    return (
+      <section className='page-contact' id='contact'>
+        <h2>conTAcT</h2>
+        <div className='contact-icons'>
+          <ul>
+            <li><LinkedInLogo /></li>
+            <li><GitHubLogo /></li>
+            <li><GmailLogo /></li>
+          </ul>
+        </div>
+        <form className='contact-email'>
+          <span className={contactNameClass}>
+            <input
+              className='input__field input__field--haruki'
+              type="text"
+              id="input-1"
+              onChange={this.handleNameChange}
+            />
+            <label className="input__label input__label--haruki" htmlFor="input-1">
+              <span className="input__label-content input__label-content--haruki">First Name</span>
+            </label>
+          </span>
+        </form>
+      </section>
+    )
+  }
+}
 
 export default Contact
