53d6d25f3549b455b1eba3ca4df3b2d5ccbe648d
Matt Szabo <wekilledit@users.noreply.github.com>
2016-08-01 11:39:07 +1000

    large screen anch tag cursor change on hover

--- app/src/About/pages/AboutPage4.js
+++ app/src/About/pages/AboutPage4.js
@@ -5,19 +5,19 @@ const AboutPage4 = () => {
     <div className='subpage-about'>
       <h3>This porTfoLio</h3>
       <p>
-        This responsive single page app demonstates much of what I have learned so far.
+        This responsive single page app demonstates much of what I have learned so far. It utilizes vw and vh style units in an attempt to have a consistent look and feel across all screen sizes.
       </p>
       <h4>developmenT environmenT</h4>
       <p>
-        The app was developed using webpack (configured with hot module replacement), git & github for source control, npm for package management and task running, chrome and React dev tools for debugging.
+        The app is developed using webpack (configured with hot module replacement), git & github for source control, npm for package management and task running, and Chrome & React dev tools for debugging.
       </p>
       <h4>coding</h4>
       <p>
-        The app uses React components written in JavaScript ES6 and JSX, which was all transpiled using babel. Sass was also used for styling and you can see my use of SVG's for the arrow on the home screen and the pagination of the carousel below.
+        The app uses React components written in JavaScript ES6 and JSX, which is all transpiled using babel. Sass is used for styling and you can see the use of SVG's for the arrows and dots used for the pagination of the carousel.
       </p>
       <h4>hosTing</h4>
       <p>
-        The app is running on a Node.js express server, which resides on an AWS EC2 instance running Ubuntu.
+        The app is running on a Node.js express server, which resides on an AWS EC2 instance running Ubuntu 14.04.
       </p>
     </div>
   );
